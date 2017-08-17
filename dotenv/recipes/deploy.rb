33abe86335fee2f8c9df63504f676b8269e8e3aa
Andre Cantero <thecantero@gmail.com>
2017-08-17 14:33:08 +0800

    Make sure the shared dir exist before creating the dotenv file

--- dotenv/recipes/deploy.rb
+++ dotenv/recipes/deploy.rb
@@ -1,29 +1,24 @@
-# import JSON custom config into .env to emulate ENV
-# example:
-# {
-#   "deploy": {
-#     "app_name" {
-#       "app_env": {
-#         "DATABASE_URL": "",
-#         "bar": "foo"
-#       }
-#     }
-#   }
-# }
-
 require 'shellwords'
+require 'fileutils'
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
 
-  Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
-  
+  Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}... in #{deploy[:deploy_to]}")
   environment_variables = deploy[:app_env].to_h.merge(deploy[:environment_variables].to_h)
+  
   begin
-    open("#{deploy[:deploy_to]}/shared/.env", 'w') do |f|
-      require 'yaml'
-      environment_variables.each do |name, value|
-        f.puts "#{name}=#{value.to_s}"
-      end
+    FileUtils::mkdir_p "#{deploy[:deploy_to]}/shared"
+    require 'yaml'
+
+    environment_variables = deploy[:app_env].to_h.merge(deploy[:environment_variables].to_h)
+
+    env_file_content = environment_variables.map{|name,value| "#{name}=#{value.to_s.shellescape}"}.join("\n")
+
+    file "#{deploy[:deploy_to]}/shared/.env" do
+      content env_file_content
+      owner "deploy"
+      group  "www-data"
+      mode 00774
     end
 
   rescue SystemCallError
