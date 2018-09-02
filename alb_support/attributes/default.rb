7129848265cdaaf7fce9d12db80fe57240e8cd44
Ker Ruben Ramos <xdiscent@gmail.com>
2018-09-02 16:10:38 +0800

    Add feature to support Application Load Balancer

---
+++ alb_support/attributes/default.rb
@@ -0,0 +1,2 @@
+node.default[:alb_helper][:connection_draining_timeout] = 750
+node.default[:alb_helper][:state_check_frequency] = 30
---
+++ alb_support/metadata.rb
@@ -0,0 +1,7 @@
+name             'alb_support'
+maintainer       'Ker Ruben Ramos'
+maintainer_email 'xdiscent@gmail.com'
+description      'AWS Application Load Balancer control.'
+version          '1.0.0'
+
+supports 'amazon'
