d4818a6658a3ae15d73c5b0ab57e768e06d07e4c
Ker Ruben Ramos <xdiscent@gmail.com>
2019-01-23 11:20:29 +0800

    nodejs

---
+++ nodejs/metadata.rb
@@ -0,0 +1,4 @@
+name        "nodejs"
+description "Installs nodejs"
+maintainer  "Ker Ruben Ramos"
+version     "1.0.0"
---
+++ nodejs/recipes/default.rb
@@ -0,0 +1,13 @@
+package %w(nodejs npm)  do
+  action :nothing
+end
+
+bash 'prepare nodejs package' do
+  code <<-EOH
+    yum remove -y nodejs npm
+    curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -'
+    EOH
+  action :run
+  notifies :install, 'package[nodejs npm]', :immediately
+  not_if 'rpm -qa | grep nodesource '
+end
