eb2faf5a4639304e1945db9ea93c7a89ff3d3dec
Ker Ruben Ramos <xdiscent@gmail.com>
2019-01-23 11:38:45 +0800

    more cleanup

--- nodejs/recipes/default.rb
+++ nodejs/recipes/default.rb
@@ -1,13 +1,9 @@
-package 'nodejs' do
-  action :nothing
-end
-
 bash 'prepare nodejs package' do
   code <<-EOH
     yum remove -y nodejs
     curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
+    yum install -y nodejs
     EOH
   action :run
-  notifies :install, 'package[nodejs]', :immediately
   not_if 'rpm -qa | grep nodesource '
 end
