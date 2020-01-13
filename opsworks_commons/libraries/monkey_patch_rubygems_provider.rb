b769ea3bc2370ff3b70e436a50ca105a14f40c7e
Ker Ruben Ramos <xdiscent@gmail.com>
2020-01-14 03:57:16 +0800

    monkeypatch

--- opsworks_commons/libraries/monkey_patch_rubygems_provider.rb
+++ opsworks_commons/libraries/monkey_patch_rubygems_provider.rb
@@ -94,6 +94,19 @@ class Chef
           # the AlternateGemEnvironment to be used (except for chef_gem)
           return true
         end
+
+        def install_via_gem_command(name, version)
+          if @new_resource.source =~ /\.gem$/i
+            name = @new_resource.source
+          else
+            src = @new_resource.source && "  --source=#{@new_resource.source} --source=http://rubygems.org"
+          end
+          if version
+            shell_out!("#{gem_binary_path} install #{name} -q --no-document -v \"#{version}\"#{src}#{opts}", :env=>nil)
+          else
+            shell_out!("#{gem_binary_path} install \"#{name}\" -q --no-document #{src}#{opts}", :env=>nil)
+          end
+        end
       end
     end
   end
