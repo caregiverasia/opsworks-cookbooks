fefe4825a6b542b7081127b8bcfa9971e11c3c87
Ker Ruben Ramos <xdiscent@gmail.com>
2020-01-14 04:24:28 +0800

    fix typo

--- nodejs/recipes/default.rb
+++ nodejs/recipes/default.rb
@@ -5,7 +5,7 @@
 bash 'prepare nodejs package' do
   code <<-EOH
     yum remove -y nodejs npm
-    curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -'
+    curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
     EOH
   action :run
   not_if 'rpm -qa | grep nodesource '
