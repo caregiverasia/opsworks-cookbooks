52b00965383787fbb6b81cfa4feb9fbb0745af46
Ker Ruben Ramos <xdiscent@gmail.com>
2020-01-15 14:37:37 +0800

    fix

--- nodejs/recipes/default.rb
+++ nodejs/recipes/default.rb
@@ -1,11 +1,8 @@
-# package %w(nodejs npm)  do
-#   action :nothing
-# end
-
 bash 'prepare nodejs package' do
   code <<-EOH
     yum remove -y nodejs npm
     curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
+    yum install -y nodejs
     EOH
   action :run
   not_if 'rpm -qa | grep nodesource '
