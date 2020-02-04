fd595eb219f13730cea541f27cbf59865d966a41
Ker Ruben Ramos <xdiscent@gmail.com>
2020-02-05 01:44:25 +0800

    fix

--- nginx/recipes/enable_default_site.rb
+++ nginx/recipes/enable_default_site.rb
@@ -1,7 +1,4 @@
 execute "nginx.nxensite default" do
   command "/usr/sbin/nxensite default"
-  if restart
-    notifies :reload, "service[nginx]"
-  end
   not_if do ::File.symlink?("#{node[:nginx][:dir]}/sites-enabled/default") end
 end
